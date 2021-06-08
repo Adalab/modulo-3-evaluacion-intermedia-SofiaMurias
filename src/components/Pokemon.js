bbbd860d8cb65e9224fd738a17d9711e433a2219
SofiaMurias <sofiamuriaslopez@gmail.com>
2021-06-08 11:11:12 +0200

    add build

--- src/components/Pokemon.js
+++ src/components/Pokemon.js
@@ -2,11 +2,14 @@ import React from "react";
 import "../stylesheet/App.scss";
 
 const Pokemon = (props) => {
+  const typesItem = props.types.map((type) => {
+    return <li className="card-list--type">{type} </li>;
+  });
   return (
     <article className="cards-list">
       <img className="cards-list--img" src={props.url} />
       <h3 className="cards-list--name"> {props.name}</h3>
-      <p className="cards-list--type"> {props.types}</p>
+      <ul className="cards-list--item">{typesItem}</ul>
     </article>
   );
 };
